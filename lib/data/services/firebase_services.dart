af137f62edb60b1d4532b9f100a5d60671df03e0
ramganesh-eduvy <ram.ganesh@eduvy.in>
2025-12-16 11:00:10 +0530

    Initialized firebase

---
+++ lib/data/services/firebase_services.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class FirebaseService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:insta_clone_mini/routes/app_pages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   runApp(const MyApp());
 }
